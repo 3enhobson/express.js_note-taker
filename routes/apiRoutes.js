d5a6f63f95fbf8ec11adf850b9e0ab138fb15c40
Ben Hobson <benhobson@Bens-MacBook-Pro.local>
2022-09-28 14:07:49 -0700

    added delete route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,15 +6,19 @@ router.get('/notes',(req, res)=> {
 });
 
 router.post('/notes',(req, res)=> {
-    const newNote = req.body;
-    console.log(newNote);
+    const newNote = {
+    id: dbJson.length + 1 ,
+    title: req.body.title,
+    text: req.body.text
+  }
     dbJson.push(newNote);
     res.json(dbJson);
     
 });
 
-router.delete('/notes', (req, res) => {
-
+router.delete('/notes/:id', (req, res) => {
+dbJson.splice(req.params.id - 1, 1)
+res.json(dbJson)
   });
 
 module.exports = router;
